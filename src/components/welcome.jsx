384caaaff322c43c91fad401aded7b04b278261e
ThathaCampos <thamires.campos@sptech.school>
2024-07-15 12:48:35 -0300

    Utilizando o useReducer

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,13 +1,18 @@
+import { useContext } from "react";
 import PlanoFundo from "../assets/planoFundo.jpg"; {/*Para poder utilizar uma imagem, vc meio que vai criar uma "Variavel"
     para o armazenamento dela*/}
 import "./welcome.css";
+import { QuizContext } from "../context/quiz";
 
 const welcome = () => {
+
+  const [quizState, dispacth] = useContext(QuizContext);
+
   return (
     <div id='welcome'>
       <h2>Seja bem-vindo!</h2>
       <p>Clique no botão abaixo para começar:</p>
-      <button>Iniciar</button>
+      <button onClick={() => dispacth({type: "CHANGE_STATE"})}>Iniciar</button>
       <img src={PlanoFundo} alt="Inicio do Quiz" />
     </div>
   )
--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -1,14 +1,32 @@
-import { createContext } from "react";
-
-{/** Fazer alterações no projeto, por meio das ações*/}
+import { createContext, useReducer } from "react";
+{/** useReducer = Fazer alterações no projeto, por meio das ações*/}
 import question from '../data/questions';
+
+const STAGES = ["start", "Playing", "End"];
+
+const initialState = {
+    gameStage: STAGES[0],
+    questions
+}
+
+const quizReducer = (state, action) => {
+
+    switch (action.type) {
+        case "CHANGE_STATE":
+            return state;
+
+            default:
+                return state;
+    }
+}
+
 
 
 export const QuizContext = createContext()
 {/**Ele não pode ser vazio */}
 
 export const QuizProvider = ({ children }) => {
-    const value = {};
+    const value = useReducer(quizReducer, initialState);
     return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 }
 
